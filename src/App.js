5a2bc6860d10d52f01ee0cb9e6f1b3cd645f884d
Dan Possehl <danielbpossehl@gmail.com>
2022-02-05 09:02:31 -0800

    added subtract

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(10);
 
+  const [subtract, setsubtract] = useState(15);
+
   return (
-    <div>
-      <button>+</button>
-    </div>
+    <>
+      <div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <div>{count}</div>
+      </div>
+      <div>
+        <button onClick={() => setsubtract(subtract - 1)}>-</button>
+        <div>{subtract}</div>
+      </div>
+    </>
   );
 };
 
