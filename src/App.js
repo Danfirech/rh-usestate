5ac64d0f920e09867d2baadc05713f43e5d3d5da
Dan Possehl <danielbpossehl@gmail.com>
2022-02-06 09:31:46 -0800

    did again

--- src/App.js
+++ src/App.js
@@ -2,59 +2,30 @@ import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(10);
-
-  const [subtract, setsubtract] = useState(15);
-
-  const [{ count2, count3 }, setnewcount] = useState({
-    count2: 20,
-    count3: 30,
-  });
+  const [count2, setCount2] = useState(10);
 
   const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
-      <div>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <div>{count}</div>
-      </div>
-      <div>
-        <button onClick={() => setsubtract(subtract - 1)}>-</button>
-        <div>{subtract}</div>
-      </div>
+      <button onClick={() => setCount(count + 1)}>Plus</button>
+      <div>{count}</div>
+      <button onClick={() => setCount2(count2 - 1)}>Minus</button>
+      <div>{count2}</div>
 
-      <div>
-        <button
-          onClick={() =>
-            setnewcount((currentState) => ({
-              ...setnewcount,
-              count2: currentState.count2 + 1,
-              count3: currentState.count3 + 2,
-            }))
-          }
-        >
-          Try Me 1
-        </button>
-        <div>count 2: {count2}</div>
-        <div>count 3: {count3}</div>
-      </div>
-
-      <div>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <input
-          type="password"
-          name="password"
-          valie={password}
-          onChange={(e) => setpassword(e.target.value)}
-        />
-      </div>
+      <input
+        name="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        name="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
     </>
   );
 };
