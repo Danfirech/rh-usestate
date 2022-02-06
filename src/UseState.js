433a25619a0f7b90b7bf96e9093d69000736e7b6
Dan Possehl <danielbpossehl@gmail.com>
2022-02-06 10:36:20 -0800

    working with state

--- src/UseState.js
+++ src/UseState.js
@@ -1,31 +1,60 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const ContainerTop = styled.div`
+  width: 100vw;
+  height: 50vh;
+  background-color: black;
+  display: flex;
+`;
+const ContainerBottom = styled.div`
+  width: 100vw;
+  height: 50vh;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  width: 150px;
+  height: 150px;
+  color: blue;
+`;
+
+const Answer = styled.div`
+  color: white;
+`;
 
 const App = () => {
   const [count, setCount] = useState(10);
   const [count2, setCount2] = useState(10);
+  const [reset, resetCount] = useState(0);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Plus</button>
-      <div>{count}</div>
-      <button onClick={() => setCount2(count2 - 1)}>Minus</button>
-      <div>{count2}</div>
+      <ContainerTop>
+        <Button onClick={() => setCount(count + 1)}>Plus</Button>
+        <Answer>{count}</Answer>
+        <Button onClick={() => setCount2(count2 - 1)}>Minus</Button>
+        <Answer>{count2}</Answer>
+        <Button onClick={() => setCount(count == 0)}>Reset</Button>
+      </ContainerTop>
 
-      <input
-        name="email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <ContainerBottom>
+        <input
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </ContainerBottom>
     </>
   );
 };
