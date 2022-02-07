0cd732c08e36830966991de63b53518d0eb9c039
Dan Possehl <danielbpossehl@gmail.com>
2022-02-07 09:35:39 -0800

    styled top

--- src/UseState.js
+++ src/UseState.js
@@ -6,6 +6,8 @@ const ContainerTop = styled.div`
   height: 50vh;
   background-color: black;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const ContainerBottom = styled.div`
   width: 100vw;
@@ -17,20 +19,17 @@ const Button = styled.button`
   width: 150px;
   height: 150px;
   color: blue;
+  font-size: 50px;
 `;
 
 const Answer = styled.div`
   color: white;
+  font-size: 40px;
 `;
-
-const Hello = () => {
-  this.setCount.useState(50);
-};
 
 const App = () => {
   const [count, setCount] = useState(10);
   const [count2, setCount2] = useState(10);
-  const [reset, reset1] = useState(50);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -38,17 +37,14 @@ const App = () => {
   return (
     <>
       <ContainerTop>
-        <Button onClick={() => setCount(count + 1)}>Plus</Button>
+        <Button onClick={() => setCount(count + 1)}>+</Button>
         <Answer>{count}</Answer>
-        <Button onClick={() => setCount2(count2 - 1)}>Minus</Button>
+        <Button onClick={() => setCount2(count2 - 1)}>-</Button>
         <Answer>{count2}</Answer>
-        <div>
-          <Button onClick={() => reset1(this.count + 3)}>Reset</Button>
-        </div>
       </ContainerTop>
 
       <ContainerBottom>
-        {/* <input
+        <input
           name="email"
           type="email"
           value={email}
@@ -59,7 +55,7 @@ const App = () => {
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        /> */}
+        />
       </ContainerBottom>
     </>
   );
